b0b87986b224df1598aba88622fda0ed1dc811b0
stephane <stefb12@gmail.com>
2020-10-30 18:05:52 +0100

    a decent basic version with typos/README.md cleanup

--- tf_aws_ec2_basic/main.tf
+++ tf_aws_ec2_basic/main.tf
@@ -1,8 +1,16 @@
+# Basic EC2 instance creation
+
+# Assign Elastic IP
+# Configure with existing keypair
+# Attach existing Security Group
+#
+
+
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.12.0"
+      version = ">= 3.12"
     }
   }
 }
@@ -16,7 +24,14 @@ provider "aws" {
 resource "aws_instance" "sbe-ubuntu2004-test" {
   ami           = var.aws_ami_ubuntu2004
   instance_type = "t2.micro"
+  key_name = var.key              # leverages existing key.pem 
+  security_groups = [var.aws_sg]  # assumes the specified group is already defined in AWS
   tags = {
     Name = "sbe-ubuntu2004-test"
+    Owner = "sbenfredj"
   }
+}
+
+resource "aws_eip" "sbe-ubuntu2004-test" {
+  instance = aws_instance.sbe-ubuntu2004-test.id
 }
--- tf_aws_ec2_basic/variables.tf
+++ tf_aws_ec2_basic/variables.tf
@@ -9,3 +9,10 @@ variable "aws_region" {
 variable "aws_ami_ubuntu2004" {
      default = "ami-06fd8a495a537da8b"
 }
+
+variable "key" {
+     description = "Existing keypair to use"
+}
+variable "aws_sg" {
+     description = "Existing Security Group to use"
+}
